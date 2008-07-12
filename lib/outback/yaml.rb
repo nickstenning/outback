14a12a815701f31548f32da0f2b026c1e169a184
Nick Stenning <nick@whiteink.com>
2008-07-12 16:02:37 +0100

    DRY up Yaml support

--- lib/outback/yaml.rb
+++ lib/outback/yaml.rb
@@ -12,8 +12,12 @@ module Outback
     end
     
     def self.load_file( yaml_file )
-      yaml = File.open(yaml_file).read
-      self.load(yaml)
+      manager = Manager.new
+      tasks = ::YAML.load_file(yaml_file)
+      tasks.each do |task|
+        manager.tasks << Outback::ShellTask.new(*task)
+      end
+      return manager
     end
               
   end
